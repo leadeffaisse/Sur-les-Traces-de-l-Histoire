a8a12d47f9dc9a14cad4726749662ac970872057
Léa Deffaisse <l.deffaisse@laposte.net>
2025-04-29 21:39:40 +0200

    feat(fetch): ajout d'une boucle for pour faire 10 joke

--- js/feed.js
+++ js/feed.js
@@ -3,22 +3,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const refreshIcon = document.getElementById("refresh");
     refreshIcon.addEventListener("click", () => {
-requestJoke();
+        requestJoke();
     });
 });
 
 function requestJoke() {
-    fetch("https://v2.jokeapi.dev/joke/Any?lang=fr")
-        .then(res => res.json())
-        .then(joke => {
-            addJoke(joke)
-        }
-    );
+    const container = document.getElementById("joke-container");
+    container.innerHTML = "";
+
+    for (let i = 0; i < 10; i++) {
+        fetch("https://v2.jokeapi.dev/joke/Any?lang=fr")
+            .then(res => res.json())
+            .then(joke => {
+                const bloc = addJoke(joke);
+                container.appendChild(bloc);
+            })
+            .catch(err => console.error("Erreur dans la génération des blagues :", err));
+    }
 }
 
 function addJoke(joke) {
-    const container = document.getElementById("joke-container");
-    container.innerHTML = "";
     const bloc = document.createElement("div");
     bloc.className = "joke-box";
 
@@ -30,5 +34,5 @@ function addJoke(joke) {
     } else {
         bloc.innerHTML = `<p class="single">${joke.joke}</p>`;
     }
-    container.appendChild(bloc);
+    return bloc;
 }
